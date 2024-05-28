e9c216961607f5f34edb5f356656740b9c444ccf
Kim Moberg <kimskogfelter@outlook.com>
2024-05-28 12:45:49 +0000

    created the sign up form

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://pp5-nerdalert-drf-api-95b8bddc0859.herokuapp.com/'
+axios.defaults.headers.post['Content-Type'] = 'multipart/form/data'
+axios.defaults.withCredentials = true
