0897cf2a402c4f9ff99e69cf2021afbdc8593c6d
Kim Moberg <kimskogfelter@outlook.com>
2024-05-28 09:33:07 +0000

    css for main app and NavBar added

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from '../styles/Navbar.module.css';
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="110" />
@@ -12,13 +13,13 @@ const NavBar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
-            <Nav.Link>
+            <Nav.Link className={styles.CustomNavLink}>
               <i className="fas fa-home"></i>Home
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link  className={styles.CustomNavLink}>
               <i className="fas fa-sign-in-alt"></i>Sign in
             </Nav.Link>
-            <Nav.Link>
+            <Nav.Link  className={styles.CustomNavLink}>
               <i className="fas fa-user-plus"></i>Sign up
             </Nav.Link>
           </Nav>
